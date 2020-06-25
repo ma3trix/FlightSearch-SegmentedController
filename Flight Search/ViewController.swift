b9bfb5fc7334967aa5a4b17e48284695a3def44c
Malik <mefimihan@yahoo.com>
2020-06-25 02:02:12 -0400

    initail commit

---
+++ Flight Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flight Search
+//
+//  Created by Malik Adebiyi on 2020-06-25.
+//  Copyright © 2020 dotSandbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
